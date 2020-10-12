432547d346eed693f9acc7326192a04384dabac4
edin93 <il_matador@hotmail.fr>
2020-10-12 16:00:54 +0100

    task 3 done

--- scripts.js
+++ scripts.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
         }
     });
 
-    // Popular Tutorials quotes
+    // Popular Tutorials
     let tutorialsURL = 'https://smileschool-api.hbtn.info/popular-tutorials';
     $.ajax({
         url: tutorialsURL,
@@ -93,6 +93,67 @@ $(document).ready(function() {
                 </div>
                 `);
                 $("#tutorial-cards-container").append($html);
+            }
+        },
+        error: function(xhr, status) {
+            console.log(`An error occured`);
+        }
+    });
+
+    // Latest videos
+    let latestVideosURL = 'https://smileschool-api.hbtn.info/latest-videos';
+    $.ajax({
+        url: latestVideosURL,
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            action: 'query',
+            list: 'search',
+            format: 'json',
+        },
+        beforeSend: function() {
+            $('#latest-videos-loader').show();
+        },
+        success: function(response) {
+            let data = response;
+            console.log(data);
+            $('#latest-videos-loader').hide();
+            for (let i = 0; i < data.length; i++) {
+                let $html = (`
+                <div class="text-center col-12 col-sm-6 col-md-3">
+                    <div class="carousel-item active">
+                        <div class='tutorial-video-top'>
+                            <img class="w-100" src=${data[i].thumb_url} alt="smile image">
+                            <img src="./images/play.png" class="video-play-img" alt="video play button">
+                        </div>
+                        <div class="mx-1">
+                            <div class="font-weight-bold text-dark text-left mt-3">
+                                ${data[i].title}
+                            </div>
+                            <div class="text-secondary text-left mt-3 mb-3">
+                                ${data[i]['sub-title']}
+                            </div>
+                            <div class="d-flex align-items-center mb-3">
+                                <img src=${data[i].author_pic_url} class="rounded-circle mr-3 video-carousel-img-profile" alt="profile image">
+                                <div class="purple-text font-weight-bold">${data[i].author}</div>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex pt-1">
+                                    <img src="./images/star_on.png" class="mr-1 carousel-star-icon" alt="star icon filled in purple">
+                                    <img src="./images/star_on.png" class="mr-1 carousel-star-icon" alt="star icon filled in purple">
+                                    <img src="./images/star_on.png" class="mr-1 carousel-star-icon" alt="star icon filled in purple">
+                                    <img src="./images/star_on.png" class="mr-1 carousel-star-icon" alt="star icon filled in purple">
+                                    <img src="./images/star_off.png" class="carousel-star-icon" alt="star icon filled in grey">
+                                </div>
+                                <div class="purple-text font-weight-bold">
+                                    ${data[i].duration}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `);
+                $("#latest-videos-cards-container").append($html);
             }
         },
         error: function(xhr, status) {
